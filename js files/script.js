cc287332be1fd2623304bb55410d09ef4604e085
Victoria Lamy <victoriawebdesigns@gmail.com>
2025-11-06 12:59:02 -0500

    added js to script.js

---
+++ js files/script.js
@@ -0,0 +1,4 @@
+document.getElementById("openPlanner").addEventListener("click", () => {
+    document.getElementById("cover").classList.remove("active");
+    document.getElementById("planner").classList.add("active");
+});
